44993840cdbf1224ce4e41d2b7b650ca8175c9ac
Ing-Dom <dom@ing-dom.de>
2024-03-31 16:41:51 +0200

    added LedModule, Presence and Sensor libs

--- include/versions.h
+++ include/versions.h
@@ -1,11 +1,11 @@
 #pragma once
 
-#define MAIN_Version "b6f17bd"
+#define MAIN_Version "c93b21e"
 #define KNX_Version "9d7c8ac"
 #define MODULE_Common_Version "1.1.1+d57d41a"
 #define MODULE_Common_ETS 17
-#define MODULE_LogicModule_Version "3.1.4+cbc8707"
-#define MODULE_LogicModule_ETS 49
+#define MODULE_LogicModule_Version "3.2.0+2bc924f"
+#define MODULE_LogicModule_ETS 50
 #define MODULE_VirtualButton_Version "0.3.0+b46da5a"
 #define MODULE_VirtualButton_ETS 3
 #define MODULE_FileTransferModule_Version "0.0.4+0ba2086"
