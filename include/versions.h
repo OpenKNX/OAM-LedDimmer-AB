9eb8c05268783bd3c83d4148ae413d871554a468
Ing-Dom <dom@ing-dom.de>
2024-04-02 09:40:03 +0200

    included wip version if BinaryInputModule

--- include/versions.h
+++ include/versions.h
@@ -1,11 +1,13 @@
 #pragma once
 
-#define MAIN_Version "4499384"
+#define MAIN_Version "c357ea0"
 #define KNX_Version "9d7c8ac"
 #define MODULE_Common_Version "1.1.1+2e62b0d"
 #define MODULE_Common_ETS 17
 #define MODULE_LogicModule_Version "3.2.0+2bc924f"
 #define MODULE_LogicModule_ETS 50
+#define MODULE_BinaryInput_Version "0.0.1+dd036eb"
+#define MODULE_BinaryInput_ETS 0
 #define MODULE_VirtualButton_Version "0.3.0+b46da5a"
 #define MODULE_VirtualButton_ETS 3
 #define MODULE_FileTransferModule_Version "0.0.4+0ba2086"
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include "OpenKNX.h"
 #include "Logic.h"
-//#include "BinaryInputModule.h"
+#include "BinaryInputModule.h"
 #include "VirtualButtonModule.h"
 #include "FileTransferModule.h"
 
@@ -13,11 +13,11 @@ void setup()
 
     uint8_t gpioPins[BI_ChannelCount] = {BI_PIN_A, BI_PIN_B, BI_PIN_C, BI_PIN_D};
 
-    //openknxBinaryInputModule.setPins(gpioPins);
+    openknxBinaryInputModule.setPins(gpioPins);
 
     
     openknx.addModule(1, openknxLogic);
-    //openknx.addModule(2, openknxBinaryInputModule);
+    openknx.addModule(2, openknxBinaryInputModule);
     openknx.addModule(3, openknxVirtualButtonModule);
     openknx.addModule(9, openknxFileTransferModule);
     openknx.setup();
