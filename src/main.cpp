5df9da62e712ae5ee867d8b71305d5663ad6b9be
Andreas Breitschopp <a-breitschopp@ab-tools.com>
2025-04-27 08:21:05 +0200

    Moved temperature reading to module.

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,6 @@
 // #include "SensorDevices.h"
 // #include "SensorModule.h"
 
-#include <Temperature_LM75_Derived.h>
-
 Generic_LM75_9_to_12Bit_OneShot mytemperature(OPENKNX_LED_TEMPSENS_ADDR);
 
 void setup()
@@ -30,19 +28,9 @@ void setup()
     openknx.addModule(7, openknxLedModule);
 
     openknx.setup();
-
-    OPENKNX_LED_TEMPSENS_WIRE.setSCL(OPENKNX_LED_TEMPSENS_PIN_SCL);
-    OPENKNX_LED_TEMPSENS_WIRE.setSDA(OPENKNX_LED_TEMPSENS_PIN_SDA);
-    OPENKNX_LED_TEMPSENS_WIRE.begin();
 }
 
 void loop()
 {
     openknx.loop();
-    if(millis()%2000 == 357)
-    {
-        Serial.print("Temperature 1 = ");
-        Serial.print(mytemperature.readTemperatureC());
-        Serial.println(" C");
-    }
 }
