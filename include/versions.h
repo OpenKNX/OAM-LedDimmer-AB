6e9a68f8de2f079f27e9aa12441abfd1c0d4a2c5
Andreas Breitschopp <a-breitschopp@ab-tools.com>
2025-05-13 22:30:34 +0200

    Pre-Release 0.1.

--- include/versions.h
+++ include/versions.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define MAIN_Version "7e8fa8f"
+#define MAIN_Version "b903188"
 #define KNX_Version "2.1.2+764900e"
 #define MODULE_Common_Version "1.3.0+9ed04ad"
 #define MODULE_Common_Version_Major 1
@@ -32,7 +32,7 @@
 #define MODULE_FileTransferModule_Version_Minor 1
 #define MODULE_FileTransferModule_Version_Revision 0
 #define MODULE_FileTransferModule_ETS 1
-#define MODULE_LedModule_Version "0.2.0-DEV+a7c0fbc"
+#define MODULE_LedModule_Version "0.2.0-DEV+53ef45b"
 #define MODULE_LedModule_Version_Major 0
 #define MODULE_LedModule_Version_Minor 2
 #define MODULE_LedModule_Version_Revision 0
